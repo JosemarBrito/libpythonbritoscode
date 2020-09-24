d0954a9ba78cfc1bbefc9842395fc33e3a068c78
JosemarBrito <estudos.josemarbrito@gmail.com>
2020-09-23 23:15:21 -0300

    Corrigido erro de digitação no setup.py

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ setup(
     version=VERSION,
     description=DESCRIPTION,
     long_description=read('README.md'),
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license=read('LICENSE'),
