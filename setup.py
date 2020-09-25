baf87a4f883aab118a7c93d22e0ccfe0f780d83b
JosemarBrito <estudos.josemarbrito@gmail.com>
2020-09-25 20:45:51 -0300

    Alterando nome do package dentro do setup.py

--- setup.py
+++ setup.py
@@ -25,12 +25,12 @@ standard_exclude_directories = [
 # you can't import this from another package, when you don't know if
 # that package is installed yet.
 def find_package_data(
-        where=".",
-        package="",
-        exclude=standard_exclude,
-        exclude_directories=standard_exclude_directories,
-        only_in_packages=True,
-        show_ignored=False):
+    where=".",
+    package="",
+    exclude=standard_exclude,
+    exclude_directories=standard_exclude_directories,
+    only_in_packages=True,
+    show_ignored=False):
     """
     Return a dictionary suitable for use in ``package_data``
     in a distutils ``setup.py`` file.
@@ -60,17 +60,17 @@ def find_package_data(
                 bad_name = False
                 for pattern in exclude_directories:
                     if (fnmatchcase(name, pattern)
-                            or fn.lower() == pattern.lower()):
+                    or fn.lower() == pattern.lower()):
                         bad_name = True
                         if show_ignored:
                             print > sys.stderr, (
-                                    "Directory %s ignored by pattern %s"
-                                    % (fn, pattern))
+                                "Directory %s ignored by pattern %s"
+                                % (fn, pattern))
                         break
                 if bad_name:
                     continue
                 if (os.path.isfile(os.path.join(fn, "__init__.py"))
-                        and not prefix):
+                    and not prefix):
                     if not package:
                         new_package = name
                     else:
@@ -83,26 +83,27 @@ def find_package_data(
                 bad_name = False
                 for pattern in exclude:
                     if (fnmatchcase(name, pattern)
-                            or fn.lower() == pattern.lower()):
+                        or fn.lower() == pattern.lower()):
                         bad_name = True
                         if show_ignored:
-                            print > sys.stderr, (
-                                    "File %s ignored by pattern %s"
-                                    % (fn, pattern))
+                            print >> sys.stderr, (
+                                "File %s ignored by pattern %s"
+                                % (fn, pattern))
                         break
                 if bad_name:
                     continue
-                out.setdefault(package, []).append(prefix + name)
+                out.setdefault(package, []).append(prefix+name)
     return out
 
 
-PACKAGE = "libpythonpro"
+PACKAGE = "libpythonbritoscode"
 NAME = PACKAGE
 DESCRIPTION = "Repositório para estudos de ferramentas Pytool do curso PythonPro"
 AUTHOR = "Josemar Brito"
 AUTHOR_EMAIL = "estudos.josemarbrito@gmail.com"
 URL = "https://github.com/JosemarBrito/libpythonbritoscode"
 VERSION = __import__(PACKAGE).__version__
+
 
 setup(
     name=NAME,
